5c28c2b81b39e86d88ee1b44d22f983030f17fce
jirum <monteclar.jerome19@gmail.com>
2022-07-01 10:02:10 +0800

    html basics

---
+++ test.rb
@@ -0,0 +1,37 @@
+# def find_greater_than_5(numbers)
+#counter = 0
+#  for i in 0..numbers.length-1 do
+#    if numbers[i] > 5
+#      counter+=1
+#    end
+#  end
+#  return counter
+#end
+
+# p find_greater_than_5([10, 2, 3, 4, 5, 6, 7])
+
+# def find_greater_than_5(num)
+#   counter = 0
+#   num.each do |a|
+#     if a > 5
+#       counter +=1
+#     end
+#   end
+#   return counter
+# end
+# p find_greater_than_5([5, 6, 7, 8])
+# 
+
+# def sum_of_array(num)
+#   sum=0
+#   num.each do |a|
+#     sum = sum+a
+#   end
+#   return sum
+#
+# end
+#
+# p sum_of_array([20,21,22])
+
+
+
